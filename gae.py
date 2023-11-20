5bd2ad48d7819934b51db7d158e1825013f9fdf6
akashktesla <akashktesla@gmail.com>
2023-11-21 00:19:58 +0530

    :)

--- gae.py
+++ gae.py
@@ -1,10 +1,12 @@
 import torch
+import networkx as nx
+from torch_geometric.datasets import Planetoid
 from torch_geometric.nn import GCNConv
 from torch_geometric.utils import train_test_split_edges
 
 dataset = Planetoid("datasets","CiteSeer")
 
-
+G = nx.Graph(dataset.data)
 
 
 
---
+++ networkx_testing.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+edge_list = [(1,2),(2,4),(3,4),(7,8),(9,1)]
+G.add_edges_from(edge_list)
+# G.add_edge(1,2, weight = 0.6)
+nx.draw_spring(G,with_labels = True)
+plt.show()
+# G = nx.DiGrap()
+# G = nx.MultiGrap()
+# G = nx.MultiDiGrap()
