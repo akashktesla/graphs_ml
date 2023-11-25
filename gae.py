4abdbd5d64cb96df69e9765a2377e3c9159e2ea4
akashktesla <akashktesla@gmail.com>
2023-11-25 23:50:34 +0530

    :)

--- gae.py
+++ gae.py
@@ -5,8 +5,16 @@ from torch_geometric.nn import GCNConv
 from torch_geometric.utils import train_test_split_edges
 
 dataset = Planetoid("datasets","CiteSeer")
+print(dataset.data)
+print(dataset.data.edge_index)
+ei_x = dataset.data.edge_index[0]
+ei_y = dataset.data.edge_index[1]
+len_ei = len(edge_index_y)
+for i in range(len_ei): 
+    print(ei_x[i],ei_y[i])
 
-G = nx.Graph(dataset.data)
+# G = nx.Graph(dataset.data)
+
 
 
 
