044b4370b4f12f3cfb807b57e3fbcc0c0d243542
akashktesla <akashktesla@gmail.com>
2023-11-27 02:06:27 +0530

    :)

--- networkx_testing.py
+++ networkx_testing.py
@@ -6,7 +6,6 @@ edge_list = [(1,2),(2,4),(3,4),(7,8),(9,1)]
 G.add_edges_from(edge_list)
 # G.add_edge(1,2, weight = 0.6)
 nx.draw_spring(G,with_labels = True)
-plt.show()
-# G = nx.DiGrap()
+
 # G = nx.MultiGrap()
 # G = nx.MultiDiGrap()
